9723fe5e90551afa9422ac75f6a576c10d4c51bb
Brian Snyder <bsnyder74@gmail.com>
2024-08-26 13:33:31 -0700

    format

--- src/utils/flagsmith.ts
+++ src/utils/flagsmith.ts
@@ -1,7 +1,7 @@
 import Flagsmith from "flagsmith-nodejs";
 
 const flagsmith = new Flagsmith({
-  environmentKey: process.env.FLAGSMITH_KEY as string, //"XEb8nPDveNzjwKeSniGrW9", 
+  environmentKey: process.env.FLAGSMITH_KEY as string, //"XEb8nPDveNzjwKeSniGrW9",
   requestTimeoutSeconds: 60,
 });
 
