314155d5a1b1fc14f7c506c9411572b2ac98bed9
Brian Snyder <bsnyder74@gmail.com>
2024-08-26 14:40:18 -0700

    dumbest

--- src/utils/flagsmith.ts
+++ src/utils/flagsmith.ts
@@ -1,14 +1,8 @@
 import Flagsmith from "flagsmith-nodejs";
 
 const flagsmith = new Flagsmith({
-  environmentKey: process.env.FLAGSMITH_KEY as string, //"XEb8nPDveNzjwKeSniGrW9",
+  environmentKey: "XEb8nPDveNzjwKeSniGrW9", //process.env.FLAGSMITH_KEY as string,
   requestTimeoutSeconds: 60,
 });
 
 export default flagsmith;
-
-// const Flagsmith = require('flagsmith-nodejs');
-
-// const flagsmith = new Flagsmith({
-//  environmentKey: 'FLAGSMITH_SERVER_SIDE_ENVIRONMENT_KEY',
-// });
