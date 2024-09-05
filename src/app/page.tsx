8b5095d29f7387b2235870d2e80526a09bbfdc82
Brian Snyder <bsnyder74@gmail.com>
2024-09-05 11:12:54 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
-        <p>new test here</p>
+        <p>new textx</p>
       </div>
       <Link href="/about">About</Link>
       <br />
